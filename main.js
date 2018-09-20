b6cb1a955e7c7f2bf76662846a77b443eb16e122
Doug Enas <douglassenas@gmail.com>
2018-09-20 15:47:00 -0400

    second commit

--- main.js
+++ main.js
@@ -3,21 +3,28 @@ class Grid {
         this.row1 = document.write('<span class="row">X</span>');
         this.row2 = document.write('<span class="row">X</span>');
         this.row3 = document.write('<span class="row">X</span>');
+
     }
 
 }
 
 class Cell {
-    constructor(options) {
+    constructor(rowIndex, colIndex, cellClasses) {
         this.break = document.write('<br>')
         this.column1 = document.write('<span class="row">X</span>');
         this.column2 = document.write('<span class="row">X</span>');
         this.column3 = document.write('<span class="row">X</span>');
+        this.rowIndex = rowIndex;
+        this.colIndex = colIndex;
+        this.cellClasses = cellClasses;
     }
 }
 
 
+const newGrid = new Grid({
+    rows: 3,
+    columns: 3,
+});
 
-let placeGrid = new Grid
 let cellOne = new Cell
 let cellTwo = new Cell
